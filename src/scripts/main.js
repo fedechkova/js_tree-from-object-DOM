2e0d1cb379551f0e00762c80f32f827182823f05
fedechkova <fedechko.anya@gmail.com>
2026-03-14 17:10:37 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,41 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+    element.appendChild(li);
+
+    if (Object.keys(data[key]).length) {
+      const ul = document.createElement('ul');
+
+      li.appendChild(ul);
+      createTree(ul, data[key]);
+    }
+  }
 }
 
 createTree(tree, food);
+
+/* const tree = document.querySelector('#tree');
+
+function createTree(element, data) {
+  const ul = document.createElement('ul');
+
+  element.append(ul);
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+    ul.append(li);
+
+    if (Object.keys(data[key]).length) {
+      createTree(li, data[key]);
+    }
+  }
+}
+
+createTree(tree, food);
+ */
